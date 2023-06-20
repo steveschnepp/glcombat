117af5a06f379c84b780c1410a457c066fcaf879
Steve Schnepp <steve.schnepp@pwkf.org>
2023-06-20 19:17:32 +0200

    map: refactor map_load into pieces

--- map.c
+++ map.c
@@ -4,9 +4,6 @@
 
 #include "map.h"
 
-#define MAP_X 128
-#define MAP_Y 128
-
 static int middle(int min, int i, int max) {
 	if (i < min) return min;
 	if (i > max) return max;
@@ -24,53 +21,59 @@ static int map_get_vertice(int i, int j, int size_y) {
 	return idx + 1; // obj facets begins with 1
 }
 
-void map_load(struct map *map, char *filename, 	int i_step, int j_step)
-{
+
+static void map_readfile(struct map *map, char *filename) {
 	FILE *f = fopen(filename, "r");
-	struct map MAP = {0};
 
-	fscanf(f, "%d", &MAP.size_x);
-	fscanf(f, "%d", &MAP.size_y);
 
-	fscanf(f, "%f", &MAP.unit_x);
-	fscanf(f, "%f", &MAP.unit_y);
+	fscanf(f, "%d", &map->size_x);
+	fscanf(f, "%d", &map->size_y);
 
-	MAP.h = malloc(MAP.size_x * MAP.size_y * sizeof(float));
+	fscanf(f, "%f", &map->unit_x);
+	fscanf(f, "%f", &map->unit_y);
 
-	for (int x = 0; x < MAP.size_x; x ++) for (int y = 0; y < MAP.size_y; y ++) {
-		fscanf(f, "%f", map_get_cell(&MAP, x, y));
+	map->h = malloc(map->size_x * map->size_y * sizeof(float));
+
+	for (int x = 0; x < map->size_x; x ++) for (int y = 0; y < map->size_y; y ++) {
+		fscanf(f, "%f", map_get_cell(map, x, y));
 	}
 
 	fclose(f);
 
 	// Precompute the object
-	object_set_name(&MAP.o, filename);
+	object_set_name(&map->o, filename);
+}
 
+void static map_object_add_vertexes(struct map *map)
+{
 	// Vertexes
-	for (int i = 0; i < MAP.size_x; i++) {
-		for (int j = 0; j < MAP.size_y; j++) {
-			float x = MAP.unit_x * (i - MAP.size_x / 2);
-			float y = MAP.unit_y * (j - MAP.size_y / 2);
-			float z = *map_get_cell(&MAP, i, j);
+	for (int i = 0; i < map->size_x; i++) {
+		for (int j = 0; j < map->size_y; j++) {
+			float x = map->unit_x * (i - map->size_x / 2);
+			float y = map->unit_y * (j - map->size_y / 2);
+			float z = *map_get_cell(map, i, j);
 
 			struct v3f vertices_current = { x, y, z, };
 
-			object_add_vertex(&MAP.o, vertices_current);
+			object_add_vertex(&map->o, vertices_current);
 		}
 	}
+}
 
+void static map_object_add_faces(struct map *map, int i_step, int j_step)
+{
 	// Faces
-	for (int i = i_step; i < MAP.size_x; i+=i_step) {
-		for (int j = j_step; j < MAP.size_y; j+=j_step) {
-			int a =  map_get_vertice(i, j, MAP.size_y);
-			int b =  map_get_vertice(i-i_step, j, MAP.size_y);
-			int c =  map_get_vertice(i, j-j_step, MAP.size_y);
-			int d =  map_get_vertice(i-i_step, j-j_step, MAP.size_y);
+	for (int i = i_step; i < map->size_x; i+=i_step) {
+		for (int j = j_step; j < map->size_y; j+=j_step) {
+			int a =  map_get_vertice(i, j, map->size_y);
+			int b =  map_get_vertice(i-i_step, j, map->size_y);
+			int c =  map_get_vertice(i, j-j_step, map->size_y);
+			int d =  map_get_vertice(i-i_step, j-j_step, map->size_y);
 
-			struct v3f va = MAP.o.vertices[a-1];
-			struct v3f vb = MAP.o.vertices[b-1];
-			struct v3f vc = MAP.o.vertices[c-1];
-			struct v3f vd = MAP.o.vertices[d-1];
+			struct v3f va = map->o.vertices[a-1];
+			struct v3f vb = map->o.vertices[b-1];
+			struct v3f vc = map->o.vertices[c-1];
+			struct v3f vd = map->o.vertices[d-1];
 
 			// Compute the middle vertex
 			struct v3f ve = va;
@@ -78,7 +81,7 @@ void map_load(struct map *map, char *filename, 	int i_step, int j_step)
 			ve = v3f_add(ve, vc);
 			ve = v3f_add(ve, vd);
 			ve = v3f_div(ve, 4);
-			int e = object_add_vertex(&MAP.o, ve);
+			int e = object_add_vertex(&map->o, ve);
 
 			struct v3f ea = v3f_sub(va, ve);
 			struct v3f eb = v3f_sub(vb, ve);
@@ -90,11 +93,12 @@ void map_load(struct map *map, char *filename, 	int i_step, int j_step)
 			struct v3f ced = v3f_cross_product(ed, ec);
 			struct v3f deb = v3f_cross_product(eb, ed);
 
+
 			// Autocompute normals
-			int bea_idx = object_add_vertex_normal(&MAP.o, bea);
-			int aec_idx = object_add_vertex_normal(&MAP.o, aec);
-			int ced_idx = object_add_vertex_normal(&MAP.o, ced);
-			int deb_idx = object_add_vertex_normal(&MAP.o, deb);
+			int bea_idx = object_add_vertex_normal(&map->o, bea);
+			int aec_idx = object_add_vertex_normal(&map->o, aec);
+			int ced_idx = object_add_vertex_normal(&map->o, ced);
+			int deb_idx = object_add_vertex_normal(&map->o, deb);
 
 			{
 				struct obj_faces faces = {0};
@@ -106,7 +110,7 @@ void map_load(struct map *map, char *filename, 	int i_step, int j_step)
 				faces.face[1].normal = bea_idx;
 				faces.face[2].normal = bea_idx;
 
-				object_add_face(&MAP.o, faces);
+				object_add_face(&map->o, faces);
 			}
 
 			{
@@ -119,7 +123,7 @@ void map_load(struct map *map, char *filename, 	int i_step, int j_step)
 				faces.face[1].normal = aec_idx;
 				faces.face[2].normal = aec_idx;
 
-				object_add_face(&MAP.o, faces);
+				object_add_face(&map->o, faces);
 			}
 
 			{
@@ -132,7 +136,7 @@ void map_load(struct map *map, char *filename, 	int i_step, int j_step)
 				faces.face[1].normal = ced_idx;
 				faces.face[2].normal = ced_idx;
 
-				object_add_face(&MAP.o, faces);
+				object_add_face(&map->o, faces);
 			}
 
 			{
@@ -145,11 +149,19 @@ void map_load(struct map *map, char *filename, 	int i_step, int j_step)
 				faces.face[1].normal = deb_idx;
 				faces.face[2].normal = deb_idx;
 
-				object_add_face(&MAP.o, faces);
+				object_add_face(&map->o, faces);
 			}
 		}
 	}
+}
 
+void map_load(struct map *map, char *filename, 	int i_step, int j_step)
+{
+	struct map MAP = {0};
+
+	map_readfile(&MAP, filename);
+	map_object_add_vertexes(&MAP);
+	map_object_add_faces(&MAP, i_step, j_step);
 	object_compile_arrays(&MAP.o);
 
 	*map = MAP;
