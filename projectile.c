cfc2a9e913f4d2e4fd807bed47b99347747276e3
Steve Schnepp <steve.schnepp@pwkf.org>
2023-11-26 20:36:58 +0100

    projectile: adding drag

--- projectile.c
+++ projectile.c
@@ -26,30 +26,46 @@ struct projectile* projectile_new(struct v3f pos, struct v3f delta)
 }
 
 const struct v3f gravity = { 0, 0, -10 }; // g = 10 m/s
+const float drag_factor = 1e-6;
+
+static void projectile_remove(int idx)
+{
+    // Remove it by copying the last one here.
+    bullets[idx] = bullets[new_bullet_idx-1];
+    new_bullet_idx --;
+}
+
 
 void projectile_update_all(uint32_t time_delta_ms, struct map *map)
 {
     float time_delta_s = time_delta_ms / 1000.0;
 
+    printf("projectile_update_all");
     for(int i = 0; i < new_bullet_idx; i ++) {
+        printf(" %d", i);
         struct projectile *p = bullets + i;
         if (p->props.is_explosion) {
+            printf("e");
             p->delta.x *= 1.5;
-            if (p->delta.x > 3) {
-                // Remove it by copying the last one here.
-                p[i] = p[new_bullet_idx-1];
-                new_bullet_idx --;
-
-                // process this one
-                i --;
+            if (p->delta.x > 30.0) {
+                printf("d");
+                projectile_remove(i);
+                // reprocess this one
+                i--;
                 continue;
             }
             continue;
         }
+        printf("p");
 
         // Acceleration
         struct v3f gravity_in_time = v3f_mul(gravity, time_delta_s);
         p->delta = v3f_add(p->delta, gravity_in_time);
+
+        // Drag
+        float drag = v3f_square_magnitude(p->delta) * drag_factor;
+        struct v3f drag_in_time = v3f_mul(p->delta, drag * time_delta_s);
+        p->delta = v3f_sub(p->delta, drag_in_time);
 
         // Movement
         struct v3f pos_delta_in_time = v3f_mul(p->delta, time_delta_s);
@@ -58,9 +74,11 @@ void projectile_update_all(uint32_t time_delta_ms, struct map *map)
         // Hitting earth
         float height = map_get_height(map, p->pos.x, p->pos.y);
         if(p->pos.z < height) {
+            printf("x");
             p->props.is_explosion = 1;
-             p->delta.x = 0.1;
-             p->delta.y = p->delta.z = 0;
+            p->delta.x = 0.1;
+            p->delta.y = p->delta.z = 0;
         }
     }
+    printf(".\n");
 }
